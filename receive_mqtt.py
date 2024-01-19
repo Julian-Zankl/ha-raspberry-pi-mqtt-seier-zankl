ed39f07102f97df84b56aa61c32dc02ce08c6b30
Julian Zankl <j.zankl@care-development.com>
2024-01-19 17:20:50 +0100

    feat: added python scripts

---
+++ receive_mqtt.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from send_mqtt import get_client
+import json
+import requests
+
+device = None
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    client.subscribe("raspberry/mqtt")
+
+def on_message(client, userdata, message):
+    data = json.loads(message.payload)
+    print(data)
+    requests.post('http://10.0.0.76:8080/measurements', json=data)
+    
+
+if __name__ == "__main__":
+    client = get_client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.loop_forever()
---
+++ send_mqtt.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import paho.mqtt.client as mqtt
+from get_temperature import get_data, print_data
+import time
+import json
+import os
+import requests
+
+def create_client():
+    sensor = requests.post('http://10.0.0.76:8080/sensors', json={"name": "Snensor", "location": "Grafenschachen", "active": True, "type": "INDOOR"})
+    return sensor.json()['id']
+
+
+def get_client():
+    client = mqtt.Client()
+    if 'MQTT_USERNAME' in os.environ:
+        client.username_pw_set(
+            os.environ['MQTT_USERNAME'], os.environ['MQTT_PASSWORD'])
+    client.connect('broker.emqx.io')
+    return client
+
+
+def loop(client, sensor):
+    while True:
+        data = get_data()
+        data['sensor'] = sensor
+        print_data(data)
+        client.publish('raspberry/mqtt', json.dumps(data))
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    sensor = create_client()
+    client = get_client()
+    client.loop_start()
+    loop(client, sensor)
